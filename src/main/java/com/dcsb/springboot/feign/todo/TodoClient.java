4e1db610554c9b4030063f6bfd72f18b4384c5f1
dbscodesandbox <dbscodesandbox@gmail.com>
2022-05-18 18:43:48 +0530

    Initial commit

---
+++ src/main/java/com/dcsb/springboot/feign/todo/TodoClient.java
@@ -0,0 +1,11 @@
+package com.dcsb.springboot.feign.todo;
+
+import java.util.List;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//Todo URL https://jsonplaceholder.typicode.com/todos
+public interface TodoClient {
+
+}
